11f470fa4de6bd39fbcc492909d2cff700d913ab
Lukas <lukas.storz@afamily.de>
2025-12-24 14:34:54 +0100

    startet with initial structure and exe files

---
+++ active.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 24 14:28:42 2025
+
+@author: lukas
+"""
+
+print("Active")
---
+++ passive.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 24 14:26:13 2025
+
+@author: lukas
+"""
+
+print("Hello")
